f6e50956f96df06178d19fa4927a026497edc19f
ColdSteel Studios <coldsteelofficial@gmail.com>
2015-02-14 14:50:48 +1100

    fixed issues with dungeon generation
    need to do more work on player and camera movement, then on combat
    controls

---
+++ Assets/Scripts/DeathTimer.cs
@@ -0,0 +1,22 @@
+﻿// ---------------------------------------------------------------------------
+// DeathTimer.cs
+// 
+// Destroys the parent GameObject after a cset amount of time has passed
+// 
+// Original Author: Harley Laurie
+// ---------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class DeathTimer : MonoBehaviour
+{
+    public float m_deathTime;
+
+    void Update()
+    {
+        m_deathTime -= Time.deltaTime;
+        if (m_deathTime <= 0.0f)
+            GameObject.Destroy(this.gameObject);
+    }
+}
